7e2be8a35993b697c0294b00b0651a81e4c08ed5
GBaldomir <GBaldomir@NB-004675>
2022-06-10 17:05:48 -0300

    Agregar archivos de proyecto.

---
+++ TP5-CrearGIT/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP5_CrearGIT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //CODIGO SUBIDO TP 5 CREAR CUENTA DE GIT 
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TP5-CrearGIT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TP5_CrearGIT.MvcApplication" Language="C#" %>
